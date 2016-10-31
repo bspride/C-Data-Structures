ac4949eb8ffd778c669840328217d98156053569
Michael Clarke <bspride11@gmail.com>
2016-10-30 22:14:35 -0400

    Implement methods

--- Vector/vector.c
+++ Vector/vector.c
@@ -3,18 +3,15 @@ void vector_init(int capacity) {
   
   Vector *vector = malloc(size_of(Vector));
 
-  vector->size = 0;
+  vector->numElements = 0;
   vector->capacity = true_capacity;
-  vector->data = (int *)malloc(sizeof(int) * true_capacity);
+  vector->data = (int *)malloc(numElementsof(int) * true_capacity);
 
   return vector;
 }
 
-void resize_for_size(Vector *vector, int size) {
-  if(vector->size < size) {
-    vector->capacity *= 2;
-    vector->data = realloc(vector->data, sizeof(int) * vector->capacity);
-  } else if
+int vector_numElements(Vector *vector) {
+  return vector->numElements;
 }
 
 int determine_capacity(int capacity) {
@@ -32,8 +29,12 @@ int determine_capacity(int capacity) {
   return true_capacity;
 }
 
-int vector_size(Vector *vector) {
-  return vector->size;
+// TODO: Need to double capacity or when popping an item, if size is 1/4 of capacity, resize to half
+void resize_for_numElements(Vector *vector, int numElements) {
+  if(vector->numElements < numElements) {
+    vector->capacity *= 2;
+    vector->data = realloc(vector->data, numElementsof(int) * vector->capacity);
+  } else if
 }
 
 int vector_capacity(Vector *vector) {
@@ -41,56 +42,83 @@ int vector_capacity(Vector *vector) {
 }
 
 bool vector_is_empty(Vector *vector) {
-  return vector->size == 0 ? true : false;
+  return vector->numElements == 0 ? true : false;
 }
 
 int vector_get(Vector *vector, int index) {
-  if (index >= vector->size || index < 0 ) {
-    printf("Index %d out of bounds for vector of size %d\n", index, vector->capacity);
-    exit(1);
+  if (index >= vector->numElements || index < 0 ) {
+    printf("Index %d out of bounds for vector of numElements %d\n", index, vector->capacity);
+    exit(EXIT_FAILURE);
   }
-  return vector->data[index];
+  /* Mem address of start of array plus index of element derefenced will return value */
+  return *(vector->data + index);
 }
 
 void vector_push(Vector *vector, int value) {
-  double_capacity_if_full(vector);
+  resize_for_numElements(vector, vector->numElements + 1);
 
-  vector->data[vector->size++] = value;
+  /* Get pointer to empty space at end of array */
+  *(vector->data + vector->numElements) = value;
+  /* Increment numElements */
+  ++(vector->numElements);
 }
 
-//Need to redo this method
+// TODO: Double capacity if full, then resize array until able to insert at appropriate position, then insert
 void vector_insert(Vector *vector, int index, int value) {
-  if (index >= vector->size || index < 0 ) {
-    printf("Index %d out of bounds for vector of size %d\n", index, vector->capacity);
-    exit(1);
+  if (index < 0 ) {
+    printf("Index %d out of bounds for vector", index;
+    exit(EXIT_FAILURE);
   }
-  while(index >= vector->size ) {
-    vector_push(vector, 0);
-  }
+  resize_for_numElements(vector, vector->numElements + 1);
   vector->data[index] = value;
-  vector->size++;
+  vector->numElements++;
 }
 
 void vector_prepend(Vector *vector, int value) {
-  double_capacity_if_full(vector);
+  resize_for_numElements(vector, vector->numElements + 1);
 
-  memmove(vector->data + 1, vector->data, 1);
-  vector->data[0] = value;
-  vector->size++;
+  vector_insert(vector, 0, value);
 }
 
 int vector_pop(Vector *vector) {
-  return vector->data[vector->size--];
+  if(*vector->numElements == 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  int value = *(vector->data + vector->numElements-1);
+  vector->numElements--;
+
+  resize_for_numElements(vector, vector->numElements);
+  return value;
 }
 
 void vector_delete(Vector *vector, int index) {
+  if(index < 0 || index >= vector->numElements) {
+    exit(EXIT_FAILURE);
+  }
 
+  memmove(vector->data + index, vector->data + index + 1, (vector->numElements-index-1)*sizeof(int));
+  vector->numElements--;
+
+  resize_for_numElements(vector, vector->numElements);
 }
 
 void vector_remove(Vector *vector, int value) {
-
+  int index = vector_find(vector, value);
+  while(index != -1) {
+    vector_delete(vector, index);
+    index = vector_find(vector, value);
+  }
 }
 
 int vector_find(Vector *vector, int value) {
-  
+  int found_index = -1;
+  for(int i = 0; i < vector->numElements; i++) {
+    if(*(vector->data + i) == value) {
+      found_index = i;
+      break;
+    }
+  }
+
+  return found_index;
 }
