3e4f7ae9166d3f07e0f38b3fb0603046a4750d66
Michael Clarke <bspride11@gmail.com>
2016-11-22 21:20:43 -0500

    Implement empty and full

--- Queue/queue.c
+++ Queue/queue.c
@@ -5,3 +5,23 @@ void queue_init(queue *q, int size) {
   q->size = 0;
   q->data = (int *)malloc(size_of(int) * q->capacity);
 }
+
+// Adds item at end of available storage
+void enqueue(queue *q, int value) {
+
+}
+
+// Returns value and removes least recently added element
+int dequeue(queue *q) {
+
+}
+
+// Checks if queue is empty
+bool empty(queue *q) {
+  return q->size == 0;
+}
+
+// Check is queue is full
+bool full(queue *q) {
+  return q->size == q->capacity;
+}
