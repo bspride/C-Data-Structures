cfcea62614dc08fe13ef03f5fd58c1f18d40a6a1
Michael Clarke <bspride11@gmail.com>
2016-11-22 20:02:24 -0500

    Implement init, size, isEmpty, value_at

--- LinkedList/linkedlist.c
+++ LinkedList/linkedlist.c
@@ -1,41 +1,69 @@
+#include "linkedlist.h"
+
+void linkedlist_init(linkedlist *ll) {
+  ll->head = null;
+  ll->tail = null;
+  ll->length = 0;
+}
+
 // returns number of data elements in list
-int size(linkedlist *);
+int size(linkedlist *ll) {
+  return ll->length;
+}
 
 // bool returns true if empty
-bool isEmpty(linkedlist *);
+bool isEmpty(linkedlist *ll) {
+  return ll->length == 0;
+}
 
 // returns the value of the nth item (starting at 0 for first)
-int value_at(linkedlist *, int index);
+int value_at(linkedlist *ll, int index) {
+  node_t *current;
+
+  if(ll->head != NULL) {
+    current = ll->head;
+
+    for(int i = 0; i < index; i++) {
+      current = current->next;
+    }
+
+    if(current != NULL) {
+      return current->value;
+    }
+  }
+
+  return NULL;
+}
 
 // adds an item to the front of the list
-void push_front(linkedlist *, int value);
+void push_front(linkedlist *ll, int value);
 
 // remove front item and return its value
-int pop_front(linkedlist *);
+int pop_front(linkedlist *ll);
 
 // adds an item at the end
-void push_back(linkedlist *, int value);
+void push_back(linkedlist *ll, int value);
 
 // removes end item and returns its value
-int pop_back(linkedlist *);
+int pop_back(linkedlist *ll);
 
 // get value of front item
-int front(linkedlist *);
+int front(linkedlist *ll);
 
 // get value of an end item
-int back(linkedlist *);
+int back(linkedlist *ll);
 
 // insert value at index, so current item at that index is pointed to by new item at index
-void insert(linkedlist *, int index, int value);
+void insert(linkedlist *ll, int index, int value);
 
 // removes node at given index
-void erase(linkedlist *, int index);
+void erase(linkedlist *ll, int index);
 
 // returns the value of the node at nth position from the end of the list
-int value_n_from_end(linkedlist *, int n);
+int value_n_from_end(linkedlist *ll, int n);
 
 // reverses the list
-void reverse(linkedlist *);
+void reverse(linkedlist *ll);
 
 // removes the first item in the list with this value
-void remove_value(linkedlist *, int value);
+void remove_value(linkedlist *ll, int value);
