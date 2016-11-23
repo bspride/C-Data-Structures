8e91580b42c91413e4b1619ebed594c9f9f06d8d
Michael Clarke <bspride11@gmail.com>
2016-11-23 17:25:43 -0500

    Implement queue, dequeue and empty

--- Queue/queuell.c
+++ Queue/queuell.c
@@ -1,21 +1,52 @@
 #include "queuell.h"
 
-void queue_init(queue *q) {
+queue * queue_init() {
+  queue *q = malloc(size_of(node));
+  
   q->size = 0;
   q->head = NULL;
   q->tail = NULL;
+
+  return q;
 }
 
 // adds value at position at tail
 void enqueue(queue *q, int value) {
+  node *newNode = malloc(size_of(node));
+  newNode->value = value;
+  newNode->next = NULL;
 
+  if(q->tail) {
+    q->tail->next = newNode;
+  }
+  q->tail = newNode;
+
+  if(empty(q)) {
+    q->head = newNode;
+    q->tail = newNode;
+  }
 }
 
 // returns value and removes least recently added element (front)
 int dequeue(queue *q) {
+  if(empty(q)) {
+    exit(EXIT_FAILURE);
+  }
 
+  int value = q->head->value;
+  node *temp = q->head;
+
+  if(q->tail == q->head) {
+    q->tail = NULL;
+  }
+
+  q->head = q->head->next;
+
+  free(temp);
+
+  return value;
 }
 
 bool empty(queue *q) {
-
+  return q->head == NULL;
 }
